bc850940d8dd53c4404d9e4aa79e93d605758868
ypatrick21 <ypatrickflor@gmail.com>
2025-05-21 14:51:36 +0800

    added hamburger and back-to-top icon

--- script.js
+++ script.js
@@ -18,3 +18,22 @@
 // });
 
 // sections.forEach(section => observer.observe(section));
+
+const backToTopButton = document.getElementById("backToTop");
+
+    // Show/hide the button on scroll
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 300) {
+        backToTopButton.style.display = "block";
+      } else {
+        backToTopButton.style.display = "none";
+      }
+    });
+
+    // Scroll to top when button clicked
+    backToTopButton.addEventListener("click", () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
